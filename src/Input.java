0814c1d4a4df268088367d34cfb1898c6b780887
goose9991 <150225037+goose9991@users.noreply.github.com>
2025-01-28 17:41:25 +0000

    first commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Input{
+    public static void main(String[] args){
+        //open scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //initialize variables
+        String name;
+        int age;
+        float weight;
+        boolean smoker;
+
+        //prompt user for input
+        System.out.println("Name?");
+        name = scanner.nextLine();
+        System.out.println("Age? (in years)");
+        age = scanner.nextInt();
+        System.out.println("Weight? (in decimals)");
+        weight = scanner.nextFloat();
+        System.out.println("Smoker? (Enter true or false)");
+        smoker = scanner.nextBoolean();
+        //close scanner
+        scanner.close();
+        //output user input
+        System.out.println("User info:" + "\nName: " + name + "\nAge: " + age + "\nWeight: " + weight + "\nSmoker?: " + smoker);
+    }
+}
